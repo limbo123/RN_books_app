86f0115640a833c9b252d8ff25576ab1203ddd3b
Ovramets <oleg.ovramets@teamvoy.com>
2025-03-16 12:34:06 +0100

    feat: add fonts + firebase config for both platforms

--- App.tsx
+++ App.tsx
@@ -1,8 +1,17 @@
-import React from 'react';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
+import React, {useEffect} from 'react';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
 import AppNavigator from './src/navigation/AppNavigator';
+import remoteConfig from '@react-native-firebase/remote-config';
 
 const App = () => {
+  useEffect(() => {
+    remoteConfig()
+      .setDefaults({
+        json_data: '{}',
+      })
+      .then(() => remoteConfig().fetchAndActivate())
+      .then(fetchedRemotely => {});
+  }, []);
   return (
     <SafeAreaProvider>
       <AppNavigator />
--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -1,12 +1,12 @@
 import remoteConfig from '@react-native-firebase/remote-config';
 
-export const fetchRemoteConfig = async () => {
+export const fetchRemoteConfig = async (key: string) => {
   try {
     await remoteConfig().setDefaults({
       json_data: '{}',
     });
     await remoteConfig().fetchAndActivate();
-    const data = remoteConfig().getValue('json_data').asString();
+    const data = remoteConfig().getValue(key).asString();
     return JSON.parse(data);
   } catch (error) {
     console.error('Error fetching Remote Config:', error);
