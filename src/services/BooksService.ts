286c74dd51dd99ca252bfceca5dfac8a4ef6ccfc
Ovramets <oleg.ovramets@teamvoy.com>
2025-03-17 05:41:32 +0100

    feat: implement all queries and integrate it with store

--- src/services/BooksService.ts
+++ src/services/BooksService.ts
@@ -1,14 +1,72 @@
-import { fetchRemoteConfig } from './firebase';
+import {fetchRemoteConfig} from './firebase';
 
 export interface Book {
-  id: string;
-  title: string;
+  id: number;
+  name: string;
   author: string;
-  image: string;
+  summary: string;
   genre: string;
+  cover_url: string;
+  views: string;
+  likes: string;
+  quotes: string;
+}
+
+export interface TopBannerSlide {
+  id: number;
+  book_id: number;
+  cover: string;
+}
+
+export type YouWillLikeSectionBookId = number;
+
+export interface BooksWithCategories {
+  name: string;
+  books: Book[];
 }
 
 export const fetchBooks = async (): Promise<Book[]> => {
-  const data = await fetchRemoteConfig();
+  const data = await fetchRemoteConfig('json_data');
+  return data ? data.books : [];
+};
+
+export const fetchBooksWithCategories = async (): Promise<
+  BooksWithCategories[]
+> => {
+  const books = await fetchBooks();
+  const booksCategories = books.reduce<{[key: string]: Book[]}>((acc, curr) => {
+    if (!acc[curr.genre]) acc[curr.genre] = [];
+    acc[curr.genre].push(curr);
+    return acc;
+  }, {});
+  return Object.keys(booksCategories).map(genre => ({
+    name: genre,
+    books: booksCategories[genre],
+  }));
+};
+
+export const fetchBookById = async (
+  bookId: number,
+): Promise<Book | undefined> => {
+  const books = await fetchBooks();
+  return books.find(book => book.id === bookId);
+};
+
+export const fetchTopBannerSlides = async (): Promise<TopBannerSlide[]> => {
+  const data = await fetchRemoteConfig('json_data');
+  return data ? data.top_banner_slides : [];
+};
+
+export const fetchYouWillLikeSection = async (): Promise<Book[]> => {
+  const books = await fetchBooks();
+  const data = await fetchRemoteConfig('json_data');
+  const youWillLikeSection: number[] = data ? data.you_will_like_section : [];
+  return youWillLikeSection.map(
+    bookId => books.find(book => book.id === bookId) || ({} as Book),
+  );
+};
+
+export const fetchDetailsCarouselBooks = async (): Promise<Book[]> => {
+  const data = await fetchRemoteConfig('details_carousel');
   return data ? data.books : [];
 };
--- src/store/useBookStore.ts
+++ src/store/useBookStore.ts
@@ -1,15 +1,44 @@
-import create from 'zustand';
-import { fetchBooks, Book } from '../services/BooksService';
+import {create} from 'zustand';
+import {
+  fetchBooks,
+  Book,
+  TopBannerSlide,
+  fetchTopBannerSlides,
+  fetchYouWillLikeSection,
+  BooksWithCategories,
+  fetchBooksWithCategories,
+} from 'src/services/BooksService';
 
 interface BookState {
   books: Book[];
+  topBannerSlides: TopBannerSlide[];
+  youWillLikeSection: Book[];
+  booksWithCategories: BooksWithCategories[];
   fetchBooks: () => Promise<void>;
+  fetchTopBannerSlides: () => Promise<void>;
+  fetchYouWillLikeSection: () => Promise<void>;
+  fetchBooksWithCategories: () => Promise<void>;
 }
 
-export const useBookStore = create<BookState>((set) => ({
+export const useBookStore = create<BookState>(set => ({
   books: [],
   fetchBooks: async () => {
     const books = await fetchBooks();
-    set({ books });
+    set({books});
+  },
+  topBannerSlides: [],
+  fetchTopBannerSlides: async () => {
+    const topBannerSlides = await fetchTopBannerSlides();
+    set({topBannerSlides});
+  },
+  youWillLikeSection: [],
+  fetchYouWillLikeSection: async () => {
+    const youWillLikeSection = await fetchYouWillLikeSection();
+    set({youWillLikeSection});
+  },
+  booksWithCategories: [],
+  fetchBooksWithCategories: async () => {
+    const booksWithCategories = await fetchBooksWithCategories();
+    set({booksWithCategories});
   },
 }));
